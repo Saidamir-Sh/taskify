ca487f5da5cf04af8d291b7126b97cb3002546e1
Saidamir-Sh <saidamirpl@gmail.com>
2023-10-18 21:03:01 +0200

    reinitialize django project

--- taskify_backend/manage.py
+++ taskify_backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskify.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskify_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- taskify_backend/taskify_backend/wsgi.py
+++ taskify_backend/taskify_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for taskify project.
+WSGI config for taskify_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskify.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskify_backend.settings')
 
 application = get_wsgi_application()
