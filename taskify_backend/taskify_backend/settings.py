40ae40d3752a3e8ae9afc6f85d2a2b279a1d220a
Saidamir-Sh <saidamirpl@gmail.com>
2023-10-22 22:11:43 +0200

    accounts app

--- taskify_backend/taskify_backend/settings.py
+++ taskify_backend/taskify_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
+    'accounts'
 ]
 
 REST_FRAMEWORK = {
@@ -141,3 +142,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
