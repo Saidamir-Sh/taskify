87296f443d9d1d1fa043d769ebd1caa7b10f04bf
Saidamir-Sh <saidamirpl@gmail.com>
2023-11-02 20:40:45 +0100

    basic folder structure

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,11 +1,13 @@
+import React from 'react'
 import './App.css'
+import SignUpForm from './features/authentication/components/SignUpForm'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div>
+      <SignUpForm />
+    </div>
   )
 }
 
