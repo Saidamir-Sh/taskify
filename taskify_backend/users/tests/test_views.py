8b06d3fc29ca8ef0c254f27d3ab4713389558def
Saidamir-Sh <saidamirpl@gmail.com>
2023-11-27 22:26:29 +0100

    test register views

--- taskify_backend/users/tests/test_views.py
+++ taskify_backend/users/tests/test_views.py
@@ -38,4 +38,56 @@ class TestRegistration:
         assert response.status_code == 400
         assert 'is required' in response.data['username'][0]
 
-    
+    def test_email_required(self):
+        del register_request_data['email']
+        client = APIClient()
+        response = client.post('/api/auth/register/', register_request_data)
+        assert response.status_code == 400
+        assert 'is required' in response.data['email'][0]
+
+    def test_username_max_length(self):
+        register_request_data['username'] = 'johnnyjohnsondoe'
+        client = APIClient()
+        response = client.post('/api/auth/register', register_request_data)
+        assert response.status_code == 400
+        assert 'no more than 15 characters' in response.data['username'][0]
+
+    def test_username_regex(self):
+        register_request_data['username'] = 'joe@gmail.com'
+        client = APIClient()
+        response = client.post('/api/auth/register/', register_request_data)
+        assert response.status_code == 400
+        assert 'allowed in your username' in response.data['username'][0]
+
+login_request_data = {
+    'username': 'jhon',
+    'password': '}P-9(e,W'
+}
+
+@pytest.mark.django_db
+class TestLogin:
+    def test_can_login(self):
+        client = APIClient()
+        user = UserFactory(username=login_request_data['username'])
+        user.set_password(login_request_data['password'])
+        user.save()
+        response = client.post('/api/auth/login/', login_request_data)
+        assert response.status_code == 200
+
+    def test_can_login_with_email(self):
+        client = APIClient()
+        user = UserFactory(username=login_request_data['username'], email='jhondoe@example.com')
+        user.set_password(login_request_data['password'])
+        user.save()
+        login_request_data['username'] = 'jhondoe@example.com'
+        response = client.post('/api/auth/login/', login_request_data)
+        assert response.status_code == 200
+
+    def test_incorrect_password(self):
+        client = APIClient()
+        user = UserFactory(username=login_request_data['username'])
+        user.set_password('sdfsdfwer')
+        user.save()
+        response = client.post('/api/auth/login/', login_request_data)
+        assert response.status_code == 401
+        assert 'No active account found' in response.data['detail']
--- taskify_backend/users/urls.py
+++ taskify_backend/users/urls.py
@@ -6,6 +6,6 @@ from .views import RegisterUser, Me
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('me/', Me.as_view(), name='me'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
