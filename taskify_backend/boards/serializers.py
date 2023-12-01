82c61ce6f39d575a0982caa9c43116e569447c90
Saidamir-Sh <saidamirpl@gmail.com>
2023-12-01 20:30:31 +0100

    board serializers in progress

--- taskify_backend/boards/serializers.py
+++ taskify_backend/boards/serializers.py
@@ -10,3 +10,46 @@ from users.models import User
 from users.serializers import UserSerializer
 
 from .models import Attachment, Board, Comment, Item, Label, List, Notification
+
+class LabelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Label
+        exclude = ['board']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        exclude = ['list']
+
+class AttachmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Attachment
+        fields = '__all__'
+
+class ItemSerializer(serializers.ModelSerializer):
+    labels = LabelSerializer(many=True, read_only=True)
+    attachments = AttachmentSerializer(many=True, read_only=True)
+    assigned_to = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Item
+        exclude = ['list']
+
+    def get_assigned_to(self, obj):
+        queryset = obj.assigned_to.all()
+        return UserSerializer(queryset, many=True).data
+    
+class ListSerializer(serializers.ModelSerializer):
+    items = serializers.SerializerMethodField()
+
+    class Meta:
+        model = List
+        exclude = ['board']
+    
+    def get_items(self, obj):
+        queryset = Item.objects.filter(list=obj).order_by('order')
+        return ItemSerializer(queryset, many=True).data
