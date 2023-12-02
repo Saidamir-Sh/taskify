7677a43102f97a232635520f32e6e1f6d180a609
Saidamir-Sh <saidamirpl@gmail.com>
2023-12-02 15:38:49 +0100

    board views in progress

--- taskify_backend/boards/views.py
+++ taskify_backend/boards/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+import redis
+from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Case, Q, When
+from django.utils import timezone
+from django.utils.module_loading import import_string
+from rest_framework import generics, permissions, serializers, status
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from rest_framework.views import APIView
