8b7228f357ae22075d78210a84c4eb40f8d62c71
Saidamir-Sh <saidamirpl@gmail.com>
2023-12-03 00:28:10 +0100

    boards list view

--- taskify_backend/boards/views.py
+++ taskify_backend/boards/views.py
@@ -9,3 +9,18 @@ from rest_framework import generics, permissions, serializers, status
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from users.models import User
+from .models import Attachment, Board, Comment, Item, Label, List, Notification
+from .permissions import CanViewBoard, IsAuthorOrReadOnly
+from .serializers import (AttachmentSerializer, BoardSerializer, CommentSerializer, ItemSerializer, LabelSerializer, ListSerializer, NotificationSerializer, ShortBoardSerializer)
+
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB,
+    charset="utf-8", decode_responses=True
+)
+
+class BoardList(generics.ListCreateAPIView):
+    serializer_class = ShortBoardSerializer
+    # TODO after creating projects app
+    # permission_classes = [IsProjectMember]
+    
