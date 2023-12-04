df08333dab9aafb9cf6ebb85e6cd8b9828d87d70
Saidamir-Sh <saidamirpl@gmail.com>
2023-12-04 21:32:53 +0100

    created project model

--- taskify_backend/projects/models.py
+++ taskify_backend/projects/models.py
@@ -1,3 +1,17 @@
+from boards.models import Board
 from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+from django.utils import timezone
+from users.models import User
 
-# Create your models here.
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_project')
+    title = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(blank=True, null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    members = models.ManyToManyField(User, through="ProjectMembership", through_fields=('project', 'member'))
+    boards = GenericRelation(Board, object_id_field="owner_id", content_type_field="owner_model")
+
+    def __str__(self):
+        return self.title
+    
