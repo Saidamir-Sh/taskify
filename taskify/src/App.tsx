dd055d6d56e20a2de6e53d4ad12e105ec3203658
Saidamir-Sh <saidamirpl@gmail.com>
2023-12-22 23:09:21 +0100

    configured tailwind

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -7,6 +7,14 @@ function App() {
   return (
     <div>
       <SignUpForm />
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0">
+        </div>
+        <div>
+          <div className="text-l font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
     </div>
   )
 }
--- taskify/src/features/authentication/components/SignUpForm.tsx
+++ taskify/src/features/authentication/components/SignUpForm.tsx
@@ -1,54 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { SignUp } from '../types';
 
 function SignUpForm() {
-  const [signUpForm, setSignUpForm] = useState<SignUp>({email: '', password: ''});
+  const [signUpForm, setSignUpForm] = useState({ email: '', password: '' });
 
   const submitSignUpForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const requestData = JSON.stringify(signUpForm);
-  
+
     axios.post('http://127.0.0.1:8000/api/auth/signup/', requestData, {
       headers: {
         'Content-Type': 'application/json'
       }
     })
-    .then((response) => {
-      console.log(response.data);
-      axios.post('http://127.0.0.1:8000/api/auth/login/', requestData, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then((res) => {
-        if (res.data.access) {
-          localStorage.setItem("user", JSON.stringify(res.data));
-        }
-        console.log(res)
+      .then((response) => {
+        console.log(response.data);
+        axios.post('http://127.0.0.1:8000/api/auth/login/', requestData, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then((res) => {
+          if (res.data.access) {
+            localStorage.setItem("user", JSON.stringify(res.data));
+          }
+          console.log(res)
+        })
       })
-    })
-    .catch((error) => {
-      console.error('Error:', error); // Log any errors
-      // Handle the error or show an error message to the user
-    });
-  
+      .catch((error) => {
+        console.error('Error:', error); // Log any errors
+        // Handle the error or show an error message to the user
+      });
+
     console.log(signUpForm);
   };
-  
+
 
   return (
     <div>
+      
       <form onSubmit={submitSignUpForm}>
-        <input 
+        <input
           type='email'
           value={signUpForm.email}
-          onChange={(e) => setSignUpForm({...signUpForm, email: e.target.value})}
+          onChange={(e) => setSignUpForm({ ...signUpForm, email: e.target.value })}
           required
         ></input>
-        <input 
+        <input
           type='password'
           value={signUpForm.password}
-          onChange={(e) => setSignUpForm({...signUpForm, password: e.target.value})}
+          onChange={(e) => setSignUpForm({ ...signUpForm, password: e.target.value })}
           required
         ></input>
         <button>SignUp</button>
