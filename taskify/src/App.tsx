ff5ca7a7028296b103c81de28e032a35c4daedf3
Saidamir-Sh <saidamirpl@gmail.com>
2024-04-07 18:37:12 +0200

    clean up

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,85 +1,9 @@
-import { useState } from 'react';
 import './App.css';
-import { Accordion, AccordionDetail, AccordionTitle } from './components/Elements/Accordion';
-import { Button } from './components/Elements/Button';
-import { Rating } from './components/Elements/Rating';
-import { Tabs } from './components/Elements/Tabs';
-import { AutoComplete } from './components/Forms/AutoComplete';
-import { Input } from './components/Forms/Input';
-import mockData from './mockDatas';
-import { Tooltip } from './components/Elements/ToolTip';
-import { Modal } from './components/Elements/Modal';
 
 function App() {
-  const [rateValue, setRateValue] = useState<number>(0)
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
-
-  const closeModal = () => {
-    setIsModalOpen(false)
-  }
-
-  const openModal = () => {
-    setIsModalOpen(true)
-  }
-
-  const handleRateOnChange = (value: number) => {
-    setRateValue(value)
-  }
 
   return (
-    <div className='flex flex-col gap-4 max-w-4xl mx-auto min-h-full'>
-      <Button variant="primary" onClick={() => console.log('Clicked')}>Primary</Button>
-      <Button variant="danger" onClick={() => console.log('Clicked')}>Primary</Button>
-
-      <Tabs>
-        <Tabs.Titles items={mockData.map(({id, title}) => ({id, title}))}/>
-        <Tabs.Content items={mockData.map(({ id, content }) => ({id, content}))}/>
-      </Tabs>
-
-      <Accordion>
-        <AccordionTitle index={1}>Introduction</AccordionTitle>
-        <AccordionDetail index={1}>
-          Welcome to our platform! In this introductory tab, you'll find information
-        </AccordionDetail>
-
-        <AccordionTitle index={2}>Features</AccordionTitle>
-        <AccordionDetail index={2}>
-        Explore the extensive features of our product in this tab. From advanced analytics to seamless integration, we've got you covered.
-        </AccordionDetail>
-
-        <AccordionTitle index={3}>Benefits</AccordionTitle>
-        <AccordionDetail index={3}>
-        Discover the numerous benefits of using our services. From increased productivity to cost savings, see how we can help your business grow.
-        </AccordionDetail>
-      </Accordion>
-
-      <Input
-        id="customInput"
-        type="text"
-        inputName="customInput"
-        placeholder="Enter your password..."
-        onChange={(e) => console.log(e.target.value)}
-      />
-
-      <AutoComplete
-        id="customAutoComplete"
-        placeholder='Countries...'
-        options={["Algeria", "Bangladesh", "Congo", "Denmark"]}
-      />
-
-      <Rating totalRate={5} onChange={handleRateOnChange}/>
-
-      <Tooltip title='This is button'>
-        <Button variant="primary">Show Tooltip</Button>
-      </Tooltip>
-      <Button onClick={openModal}>Open Modal</Button>
-
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <p>This is paragraph</p>
-        <Button onClick={closeModal}>Close</Button>
-      </Modal>
-
-    </div>
+    <div></div>
   )
 }
 
